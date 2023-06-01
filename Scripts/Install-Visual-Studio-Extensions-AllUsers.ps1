18265747fb260b242935af6eece92474cd425767
samaea <32162601+samaea@users.noreply.github.com>
2023-06-01 18:48:30 +0100

    Update and rename Install-Visual-Studio-Extensions.ps1 to Install-Visual-Studio-Extensions-AllUsers.ps1

--- Scripts/Install-Visual-Studio-Extensions-AllUsers.ps1
+++ Scripts/Install-Visual-Studio-Extensions-AllUsers.ps1
@@ -1,3 +1,5 @@
+# Script to install Visual Studio extensions across all users (i.e computer-wide configuration)
+
 $VSIXInstallerPath = "$Env:ProgramFiles\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\VSIXInstaller.exe"
 $VSExtensionPath = "C:\Users\JohnDoe\Downloads\GitHub.Copilot.Vsix.1.84.0.1.vsix"
 # /a for admin, /q for silent installation and /sp to shutdown all processes that prevent VSIXInstaller from running
