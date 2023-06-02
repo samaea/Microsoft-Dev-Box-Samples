4b655eb48b610da541c3459fb7fcc5c4b75bf182
samaea <32162601+samaea@users.noreply.github.com>
2023-06-02 21:53:40 +0100

    Update Install-Visual-Studio-Extensions-AllUsers.ps1
    Enhancement to loop through all .vsix files that reside in a directory.

--- Scripts/Install-Visual-Studio-Extensions-AllUsers.ps1
+++ Scripts/Install-Visual-Studio-Extensions-AllUsers.ps1
@@ -1,8 +1,34 @@
-# NOTE:- The below script requires PowerShell to be running as an administrator
-# Script to install Visual Studio extensions across all users (i.e computer-wide configuration)
+param(
+     [Parameter()]
+     [string]$Path
+ )
 
-$VSIXInstallerPath = "$Env:ProgramFiles\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\VSIXInstaller.exe"
-$VSExtensionPath = "C:\Users\JohnDoe\Downloads\GitHub.Copilot.Vsix.1.84.0.1.vsix"
-# /a for admin, /q for silent installation and /sp to shutdown all processes that prevent VSIXInstaller from running
-$Arguments = @( "/a" , "/q", "/sp", $VSExtensionPath )
-start-process -FilePath $VSIXInstallerPath -ArgumentList $Arguments -Wait
+# Loop through all files in a directory that has the extension vsix
+function Get-VsixFiles {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$Path
+    )
+
+    Get-ChildItem -Path $Path -Filter *.vsix -Recurse
+}
+
+# Install all vsix files that were returned from the Get-VsixFiles function
+function Install-VsixFiles {
+    param(
+        [Parameter(Mandatory=$true)]
+        [string]$Path
+    )
+
+    $vsixFiles = Get-VsixFiles -Path $Path
+
+    foreach ($vsixFile in $vsixFiles) {
+        Write-Host "Installing $vsixFile"
+        # /a for admin, /q for silent installation and /sp to shutdown all processes that prevent VSIXInstaller from running
+        $Arguments = @( "/a" , "/q", "/sp", $VSExtensionPath )
+        start-process -FilePath $vsixFile -ArgumentList $Arguments -Wait
+        
+    }
+}
+
+Install-VsixFiles -Path $Path
