06250e807a9edf3e475075079a76636edbd0af44
samaea <32162601+samaea@users.noreply.github.com>
2023-06-03 01:40:50 +0100

    Update Install-Visual-Studio-Extensions-AllUsers.ps1

--- Scripts/Install-Visual-Studio-Extensions-AllUsers.ps1
+++ Scripts/Install-Visual-Studio-Extensions-AllUsers.ps1
@@ -1,34 +1,35 @@
 param(
      [Parameter()]
-     [string]$Path = "$Env:TEMP"
+     [string]$VsixFolderPath = "$Env:TEMP",
+     [string]$VSIXInstallerPath = "$Env:ProgramFiles\Microsoft Visual Studio\2022\Enterprise\Common7\IDE\VSIXInstaller.exe"
  )
 
 # Loop through all files in a directory that has the extension vsix
 function Get-VsixFiles {
     param(
         [Parameter(Mandatory=$true)]
-        [string]$Path
+        [string]$VsixFolderPath
     )
 
-    Get-ChildItem -Path $Path -Filter *.vsix -Recurse
+    Get-ChildItem -Path $VsixFolderPath -Filter *.vsix -Recurse
 }
 
 # Install all vsix files that were returned from the Get-VsixFiles function
 function Install-VsixFiles {
     param(
         [Parameter(Mandatory=$true)]
-        [string]$Path
+        [string]$VsixFolderPath
     )
 
-    $vsixFiles = Get-VsixFiles -Path $Path
+    $vsixFiles = Get-VsixFiles -VsixFolderPath $VsixFolderPath
 
-    foreach ($vsixFile in $vsixFiles) {
+    foreach ($vsixFile in $vsixFiles.fullname) {
         Write-Host "Installing $vsixFile"
         # /a for admin, /q for silent installation and /sp to shutdown all processes that prevent VSIXInstaller from running
-        $Arguments = @( "/a" , "/q", "/sp", $VSExtensionPath )
-        start-process -FilePath $vsixFile -ArgumentList $Arguments -Wait
+        $Arguments = @( "/a" , "/q", "/sp", $vsixFile)
+        start-process -FilePath $VSIXInstallerPath -ArgumentList $Arguments -Wait
         
     }
 }
 
-Install-VsixFiles -Path $Path
+Install-VsixFiles -VsixFolderPath $VsixFolderPath
